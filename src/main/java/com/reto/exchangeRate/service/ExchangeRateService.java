b9ac42f77aedca458cbce6abc886aac1c117c5d6
Gary Alberto Chavez Jimenez <gchavezj@emeal.nttdata.com>
2023-01-20 14:08:10 -0500

    feat service layer implementation

--- src/main/java/com/reto/exchangeRate/service/ExchangeRateService.java
+++ src/main/java/com/reto/exchangeRate/service/ExchangeRateService.java
@@ -1,4 +1,19 @@
 package com.reto.exchangeRate.service;
 
+
+import com.reto.exchangeRate.model.dto.ExchangeRateRequest;
+import com.reto.exchangeRate.model.dto.ExchangeRateResponse;
+import com.reto.exchangeRate.model.dto.RateRequest;
+import com.reto.exchangeRate.model.dto.RateResponse;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+
 public interface ExchangeRateService {
+    Maybe<ExchangeRateResponse> save(ExchangeRateRequest request);
+    Maybe<RateResponse> saveRate(RateRequest request);
+
+    Maybe<RateResponse> updateRate(RateRequest request);
+    Maybe<RateResponse> searchRateCurrency(String currency);
+    Observable<ExchangeRateResponse> findAllExchangeRate();
+    Observable<RateResponse> findAllRate();
 }
